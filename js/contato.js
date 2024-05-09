3cf2e02272b7648d109e8c8f12abf234bc5f0824
Paiaro <gabrielpaiarop@gmail.com>
2024-05-09 16:58:16 -0300

    Update contato.js

--- js/contato.js
+++ js/contato.js
@@ -101,12 +101,12 @@ var form = document.getElementById("formulario");
             if (Object.hasOwn(data, 'errors')) {
               status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
             } else {
-              status.innerHTML = "Oops! ocorreu um erro, Você é burro ? "
+              status.innerHTML = "Oops! ocorreu um erro, Você é burro !"
             }
           })
         }
       }).catch(error => {
-        status.innerHTML = "Oops! ocorreu um erro, Você é burro ? "
+        status.innerHTML = "Oops! ocorreu um erro, Você é burro !"
       });
     }
     formulario.addEventListener("submit", handleSubmit)
